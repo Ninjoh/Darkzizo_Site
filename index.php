bc93744dacb3ea4cb021a3effbe305dd7617dd62
Ninjoh <m.heil375@gmail.com>
2015-03-19 15:31:32 +0100

    Added basic template
    Added bootstrap, and basic copyright notice

---
+++ index.php
@@ -0,0 +1,42 @@
+<!--
+	#=======================================#
+	#	 									#
+	#	 Copyright (c) 2015 Martijn Heil	#
+	#										#
+	#			Licensed under MIT:			#
+	#		(We need to add link later)		#
+	#										#
+	#				 Email:					#
+	#		 ninjoh@technocraft-mc.nl		#
+	#										#
+	#				 Git Repo:				#
+	#		http://tinyurl.com/odsatom		#
+	#										#
+	#=======================================#
+-->
+<!DOCTYPE html>
+<html lang="en">
+		<head>
+		
+			<title>Template</title>
+			
+			<link rel="stylesheet" href="template.css">
+			<!-- Latest compiled and minified CSS -->
+			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+
+			<!-- Optional theme -->
+			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
+		</head>
+		<body>
+    
+    
+    
+    
+
+		    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+			<!-- Latest compiled and minified JavaScript -->
+			<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+		</body>
+
+</html>
